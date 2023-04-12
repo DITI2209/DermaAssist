62ef9f3afa34dc2a5fe51e99f014116f83bc7f48
Aryaman Tiwary <aryaman.tiwary@somaiya.edu>
2023-04-12 20:12:35 +0530

    ML model+UI

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ from datetime import timedelta
 import pyrebase
 from flask import Flask, redirect, url_for, flash, session
 from flask_session import Session
-
+from PIL import Image
+import numpy as np
+import skin_cancer_detection as SCD
 
 app = Flask(__name__)
 config = {
@@ -22,6 +24,7 @@ config = {
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
 storage = firebase.storage()
+# bucket = storage.bucket()
 
 @app.route('/')
 def index():
@@ -90,11 +93,41 @@ def signup():
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST':
-        file = request.files['file']
+        global file
+        file = request.files['pic']
         if file:
             # Upload the file to Firebase Storage
             storage.child("images/"+file.filename).put(file)
-            return 'Successful'
+            links=storage.child("images/"+file.filename).get_url(None)
+            inputimg = Image.open(file)
+            inputimg = inputimg.resize((28, 28))
+            img = np.array(inputimg).reshape(-1, 28, 28, 3)
+            result = SCD.model.predict(img)
+
+            result = result.tolist()
+            print(result)
+            max_prob = max(result[0])
+            class_ind = result[0].index(max_prob)
+            print(class_ind)
+            result = SCD.classes[class_ind]
+
+            if class_ind == 0:
+                info = "Actinic keratosis also known as solar keratosis or senile keratosis are names given to intraepithelial keratinocyte dysplasia. As such they are a pre-malignant lesion or in situ squamous cell carcinomas and thus a malignant lesion."
+
+            elif class_ind == 1:
+                info = "Basal cell carcinoma is a type of skin cancer. Basal cell carcinoma begins in the basal cells — a type of cell within the skin that produces new skin cells as old ones die off.Basal cell carcinoma often appears as a slightly transparent bump on the skin, though it can take other forms. Basal cell carcinoma occurs most often on areas of the skin that are exposed to the sun, such as your head and neck"
+            elif class_ind == 2:
+                info = "Benign lichenoid keratosis (BLK) usually presents as a solitary lesion that occurs predominantly on the trunk and upper extremities in middle-aged women. The pathogenesis of BLK is unclear; however, it has been suggested that BLK may be associated with the inflammatory stage of regressing solar lentigo (SL)1"
+            elif class_ind == 3:
+                info = "Dermatofibromas are small, noncancerous (benign) skin growths that can develop anywhere on the body but most often appear on the lower legs, upper arms or upper back. These nodules are common in adults but are rare in children. They can be pink, gray, red or brown in color and may change color over the years. They are firm and often feel like a stone under the skin. "
+            elif class_ind == 4:
+                info = "A melanocytic nevus (also known as nevocytic nevus, nevus-cell nevus and commonly as a mole) is a type of melanocytic tumor that contains nevus cells. Some sources equate the term mole with ‘melanocytic nevus’, but there are also sources that equate the term mole with any nevus form."
+            elif class_ind == 5:
+                info = "Pyogenic granulomas are skin growths that are small, round, and usually bloody red in color. They tend to bleed because they contain a large number of blood vessels. They’re also known as lobular capillary hemangioma or granuloma telangiectaticum."
+            elif class_ind == 6:
+                info = "Melanoma, the most serious type of skin cancer, develops in the cells (melanocytes) that produce melanin — the pigment that gives your skin its color. Melanoma can also form in your eyes and, rarely, inside your body, such as in your nose or throat. The exact cause of all melanomas isn't clear, but exposure to ultraviolet (UV) radiation from sunlight or tanning lamps and beds increases your risk of developing melanoma."
+
+            return render_template("reults.html", result=result, info=info, links=links)
     return render_template('upload.html')
 
 if __name__ == '__main__':
