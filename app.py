e7f4db545327e5c1f6bd206ed91fcc3c6e79e6c6
DITI2209 <diti.divekar@gmail.com>
2023-04-28 19:04:41 +0530

    removed extra files + app.py cleaning

--- app.py
+++ app.py
@@ -33,9 +33,9 @@ config = {
   "appId": "1:588321601903:web:e072797fc9189e9465d09a",
   "measurementId": "G-04938M5W17"
 }
+
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
-
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
 storage = firebase.storage()
@@ -49,15 +49,13 @@ def index():
 def getstarted():
     return render_template('login.html')
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods = ['GET', 'POST'])
 def login():
     if request.method == 'POST':
         global path_email
         path_email = request.form['email']
         password = request.form['password']
         user = None
-        
-        # Attempt to sign in with the given email and password
         error = False
         if not path_email or not password:
             error = True
@@ -65,12 +63,6 @@ def login():
         else:
             try:
                 user = auth.sign_in_with_email_and_password(path_email, password)
-                # session['user'] = email
-                # user1 = auth.get_user_by_email(email)
-                # id_token = user1.id_token
-
-                # store user ID token in Flask session
-                # session['firebase_user_token'] = id_token
             except auth.AuthError as e:
                 # Handle any authentication errors
                 error_code = e.detail.get('code')
@@ -83,12 +75,8 @@ def login():
                 else:
                     error = True
                     flash('An error occurred. Please try again later.')
-        
-        # If the user was successfully signed in, redirect to the upload page
         if user and not error:
             return render_template('dashboard.html')
-    
-    # If the request method is GET, render the login page
     return render_template('login.html')
 
 @app.route('/logout')
@@ -112,13 +100,13 @@ def viewdoc():
     print(doctor)
     return render_template('view_doctors.html', doctor=doctor)
 
-@app.route('/signup', methods=['GET', 'POST'])
+@app.route('/signup', methods = ['GET', 'POST'])
 def signup():
     if request.method == 'POST':
         email = request.form['email']
         global first_name, last_name
-        first_name=request.form['first_name']
-        last_name=request.form['last_name']
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
         password = request.form['password']
         confirm_password = request.form['confirm_password']
         data={
@@ -131,27 +119,22 @@ def signup():
         if password == confirm_password:
             user = auth.create_user_with_email_and_password(email, password)
             db.collection('pathologist').document(first_name).set(data)
-            # Redirect to the dashboard or homepage
             return render_template('login.html')
         elif auth.get_user_by_email(email) is not None:
-            # Handle email already exists error
             flash('Email already exists.')
         else:
-            # Handle password mismatch error
             flash('Passwords do not match.')
     else:
         return render_template('signup.html')
     
-@app.route('/doctor', methods=['GET', 'POST'])
+@app.route('/doctor', methods = ['GET', 'POST'])
 def doctor():
     if request.method == 'POST':
-        # Get form inputs
         global doctor_name
         global doctor_phone
         doctor_name = request.form['name']
         doctor_email = request.form['email']
         doctor_phone = request.form['contact']
-        # # Add data to the database
         data = {
             doctor_name: {
                 'name': doctor_name,
@@ -160,39 +143,12 @@ def doctor():
             }
         }
         db.collection('doctor').document(doctor_name).set(data)
-
         return redirect(url_for('patient1'))
     return render_template('add_doc.html')
 
-# @app.route('/patient', methods=['GET', 'POST'])
-# def patient():
-#     if request.method == 'POST':
-#         # Get form inputs
-#         global patient_name
-#         patient_name = request.form['pname']
-#         patient_email = request.form['pemail']
-#         patient_phone = request.form['pcontact']
-#         # patient_name = request.form['patient-name']
-#         # patient_email = request.form['patient-email']
-
-#         # Add data to the database
-#         data = {
-#             patient_name: {
-#                 'name': patient_name,
-#                 'email': patient_email,
-#                 'phone': patient_phone
-#             }
-#         }
-#         # db.child("doctor").child(doctor_name).child("patient").push(data)
-#         db.collection('doctor').document(doctor_name).collection('patient').document(patient_name).set(data)
-#         return redirect(url_for('upload'))
-#     return render_template('add_patient.html')
-
-@app.route('/patient1', methods=['GET', 'POST'])
+@app.route('/patient1', methods = ['GET', 'POST'])
 def patient1():
     if request.method == 'POST':
-        print(request.form)
-        # Get form inputs
         global patient_name
         global patient_email 
         global patient_phone
@@ -201,7 +157,6 @@ def patient1():
         patient_email = request.form['pemail']
         patient_phone = request.form['pcontact']
 
-        # Add data to the database
         data = {
             patient_name: {
                 'name': patient_name,
@@ -209,47 +164,38 @@ def patient1():
                 'phone': patient_phone
             }
         }
-        print(data)
         db.collection('doctor').document(doctor_name).collection('patient').document(patient_name).set(data)
-    
         return redirect(url_for('upload'))
-    n=0
-    if n==0:
-        doclist=[]
-        doct=db.collection('doctor').get()
+    n = 0
+    if n == 0:
+        doclist = []
+        doct = db.collection('doctor').get()
         for i in doct:
-            doctors=i.to_dict()
+            doctors = i.to_dict()
             doclist.append(doctors)
         return render_template('add_patient1.html', doclist=doclist)
 
-        
-    # return render_template('add_patient1.html')
-
-@app.route('/upload', methods=['GET', 'POST'])
+@app.route('/upload', methods = ['GET', 'POST'])
 def upload():
     if request.method == 'POST':
         global file
         file = request.files['pic']
         if file:
             global result
-            # Upload the file to Firebase Storage
-            storage.child("images/"+file.filename).put(file)
-            links=storage.child("images/"+file.filename).get_url(None)
+            storage.child("images/" + file.filename).put(file)
+            links=storage.child("images/" + file.filename).get_url(None)
             inputimg = Image.open(file)
             inputimg = inputimg.resize((28, 28))
             img = np.array(inputimg).reshape(-1, 28, 28, 3)
             result = SCD.model.predict(img)
-
             result = result.tolist()
             print(result)
             max_prob = max(result[0])
             class_ind = result[0].index(max_prob)
             print(class_ind)
             result = SCD.classes[class_ind]
-            # db.collection('doctor').document(doctor_name).collection('patient').document(patient_name).set({'image':links}, merge=True)
             if class_ind == 0:
                 info = "Actinic keratosis also known as solar keratosis or senile keratosis are names given to intraepithelial keratinocyte dysplasia. As such they are a pre-malignant lesion or in situ squamous cell carcinomas and thus a malignant lesion."
-
             elif class_ind == 1:
                 info = "Basal cell carcinoma is a type of skin cancer. Basal cell carcinoma begins in the basal cells — a type of cell within the skin that produces new skin cells as old ones die off.Basal cell carcinoma often appears as a slightly transparent bump on the skin, though it can take other forms. Basal cell carcinoma occurs most often on areas of the skin that are exposed to the sun, such as your head and neck"
             elif class_ind == 2:
@@ -263,24 +209,19 @@ def upload():
             elif class_ind == 6:
                 info = "Melanoma, the most serious type of skin cancer, develops in the cells (melanocytes) that produce melanin — the pigment that gives your skin its color. Melanoma can also form in your eyes and, rarely, inside your body, such as in your nose or throat. The exact cause of all melanomas isn't clear, but exposure to ultraviolet (UV) radiation from sunlight or tanning lamps and beds increases your risk of developing melanoma."
             
-
             return render_template("reults.html", result=result, info=info, links=links)
     return render_template('upload.html')
 
-@app.route('/profile', methods=['GET', 'POST'])
+@app.route('/profile', methods = ['GET', 'POST'])
 def profile():
-        # id_token = session['firebase_user_token']
-        # decoded_token = auth.verify_id_token(id_token)
-        # email = decoded_token['email']
         paths = db.collection('pathologist').get()
-        patho=[]
+        patho = []
         for path in paths:
             pathsdictionary = path.to_dict()
             patho.append(pathsdictionary)
         print(patho)
         collection_ref = db.collection('pathologist')
-
-# Filter the collection by email
+        # Filter the collection by email
         query = collection_ref.where('Email', '==', path_email)
         records = query.get()
         record = records[0].to_dict()
@@ -289,45 +230,42 @@ def profile():
 
         return render_template('path_profile.html', path_email=path_email, first_name=first_name, last_name=last_name)
 
-@app.route('/report', methods=['GET', 'POST'])
+@app.route('/report', methods = ['GET', 'POST'])
 def report():
     if request.method == 'POST':
-        
         return render_template('mail.html')
     return render_template('report.html', patient_name=patient_name,  patient_email=patient_email, patient_phone=patient_phone, result=result)
 
 newwebpage = os.path.join(os.getcwd(), "newwebpage.pdf")
 
-@app.route('/mail', methods=['GET', 'POST'])
+@app.route('/mail', methods = ['GET', 'POST'])
 def mail():
     if request.method == 'POST':
         given_phone = request.form['given_phone']
-        password = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
-        # password = request.form['pass']
-        pdf_ref = storage.child("reports/newwebpage.pdf").download(filename="newwebpage.pdf", path=".")
+        password = ''.join(random.choices(string.ascii_letters + string.digits, k = 8))
+        pdf_ref = storage.child("reports/newwebpage.pdf").download(filename = "newwebpage.pdf", path = ".")
 
-        
         # Your Twilio account SID and auth token
         account_sid = "AC14a03bed9ac15b6290d1e92b2ece8670"
         auth_token = "4e802b9f430cad6be1477c047c1ed77e"
 
-            # Create a Twilio client
+        # Create a Twilio client
         client = Client(account_sid, auth_token)
 
-            # The phone number you want to send the message to
+        # The phone number you want to send the message to
         to_number = given_phone
 
-            # The Twilio phone number you want to use as the sender
+        # The Twilio phone number you want to use as the sender
         from_number = '+16205368988'
 
-            # The message you want to send
+        # The message you want to send
         message = password
 
-            # Send the message using Twilio
+        # Send the message using Twilio
         client.messages.create(
-                to=to_number,
-                from_=from_number,
-                body=message)
+                to = to_number,
+                from_ = from_number,
+                body = message)
         
         pdf_reader = PyPDF2.PdfReader(newwebpage)
         pdf_writer = PyPDF2.PdfWriter()
@@ -356,26 +294,19 @@ def mail():
             encoders.encode_base64(attachment)
             attachment.add_header('Content-Disposition', f'attachment; filename="{encrypted_pdf_filename}"')
             msg.attach(attachment)
-
         smtp_server = "smtp.gmail.com"
         smtp_port = 587
-
         server = smtplib.SMTP(smtp_server, smtp_port)
         server.starttls()
         server.login(from_email, from_password)
         server.sendmail(from_email, to_emails, msg.as_string())
         server.quit()
-
-        # Delete the temporary files
-        # os.remove(pdf_filename)
         os.remove(encrypted_pdf_filename)
-
         return render_template('final.html')
-
     return render_template('mail.html', result=result)
 
-@app.route('/final', methods=['GET', 'POST'])
+@app.route('/final', methods = ['GET', 'POST'])
 def final():
     return render_template('final.html')
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug = True)
